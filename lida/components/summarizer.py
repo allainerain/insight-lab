4779776497619e06d12ed54d328b33b1d6c2c4c3
allainerain <allainericci@gmail.com>
2024-08-06 10:27:43 +0800

    feat: add more properties to number and category data types

--- lida/components/summarizer.py
+++ lida/components/summarizer.py
@@ -6,7 +6,7 @@ from lida.utils import clean_code_snippet, read_dataframe
 from lida.datamodel import TextGenerationConfig
 from llmx import TextGenerator
 import warnings
-
+ 
 system_prompt = """
 You are an experienced data analyst that can annotate datasets. Your instructions are as follows:
 i) ALWAYS generate the name of the dataset and the dataset_description
@@ -14,24 +14,48 @@ ii) ALWAYS generate a field description.
 iii.) ALWAYS generate a semantic_type (a single word) for each field given its values e.g. company, city, number, supplier, location, gender, longitude, latitude, url, ip address, zip code, email, etc
 You must return an updated JSON dictionary without any preamble or explanation.
 """
-
+ 
 logger = logging.getLogger("lida")
-
-
+ 
+ 
 class Summarizer():
     def __init__(self) -> None:
         self.summary = None
-
+ 
     def check_type(self, dtype: str, value):
         """Cast value to right type to ensure it is JSON serializable"""
-        if "float" in str(dtype):
-            return float(value)
-        elif "int" in str(dtype):
-            return int(value)
-        else:
-            return value
 
+        print(dtype)
+        if "float" in str(dtype):
+            try:
+                return float(value)
+            except:
+                value_dict = {}
+                for attribute, value in value.items():
+                    value_dict[attribute] = value
+                return value_dict
+ 
+        elif "int" in str(dtype):
+            try:
+                return int(value)
+            except:
+                value_dict = {}
+                for attribute, value in value.items():
+                    value_dict[attribute] = value
+                return value_dict
+        else:
+ 
+            # creating a dict from an object
+            value_dict = {}
+            for attribute, value in value.items():
+                value_dict[attribute] = value
+ 
+            print(value_dict)
+            return value_dict
+ 
     def get_column_properties(self, df: pd.DataFrame, n_samples: int = 3) -> list[dict]:
+        print("here")
+ 
         """Get properties of each column in a pandas DataFrame"""
         properties_list = []
         for column in df.columns:
@@ -42,7 +66,11 @@ class Summarizer():
                 properties["std"] = self.check_type(dtype, df[column].std())
                 properties["min"] = self.check_type(dtype, df[column].min())
                 properties["max"] = self.check_type(dtype, df[column].max())
-
+                properties["quantile"] = self.check_type(dtype, df[column].quantile([.25, .5, .75]))
+                properties["mean"] = self.check_type(dtype, df[column].mean())
+                properties["median"] = self.check_type(dtype, df[column].median())
+                properties["kurtosis"] = self.check_type(dtype, df[column].kurt())
+ 
             elif dtype == bool:
                 properties["dtype"] = "boolean"
             elif dtype == object:
@@ -54,17 +82,19 @@ class Summarizer():
                         properties["dtype"] = "date"
                 except ValueError:
                     # Check if the string column has a limited number of values
+                    # Optimizable
                     if df[column].nunique() / len(df[column]) < 0.5:
                         properties["dtype"] = "category"
                     else:
                         properties["dtype"] = "string"
             elif pd.api.types.is_categorical_dtype(df[column]):
                 properties["dtype"] = "category"
+ 
             elif pd.api.types.is_datetime64_any_dtype(df[column]):
                 properties["dtype"] = "date"
             else:
                 properties["dtype"] = str(dtype)
-
+ 
             # add min max if dtype is date
             if properties["dtype"] == "date":
                 try:
@@ -74,6 +104,10 @@ class Summarizer():
                     cast_date_col = pd.to_datetime(df[column], errors='coerce')
                     properties["min"] = cast_date_col.min()
                     properties["max"] = cast_date_col.max()
+ 
+            elif properties["dtype"] == "category":
+                properties["value_counts"] = self.check_type(dtype, df[column].value_counts())
+ 
             # Add additional properties to the output dictionary
             nunique = df[column].nunique()
             if "samples" not in properties:
@@ -87,14 +121,14 @@ class Summarizer():
             properties["description"] = ""
             properties_list.append(
                 {"column": column, "properties": properties})
-
+ 
         return properties_list
-
+ 
     def enrich(self, base_summary: dict, text_gen: TextGenerator,
                textgen_config: TextGenerationConfig) -> dict:
         """Enrich the data summary with descriptions"""
         logger.info(f"Enriching the data summary with descriptions")
-
+ 
         messages = [
             {"role": "system", "content": system_prompt},
             {"role": "assistant", "content": f"""
@@ -102,7 +136,7 @@ class Summarizer():
         {base_summary}
         """},
         ]
-
+ 
         response = text_gen.generate(messages=messages, config=textgen_config)
         enriched_summary = base_summary
         try:
@@ -114,21 +148,21 @@ class Summarizer():
             print(response.text[0]["content"])
             raise ValueError(error_msg + "" + response.usage)
         return enriched_summary
-
+ 
     def summarize(
             self, data: Union[pd.DataFrame, str],
             text_gen: TextGenerator, file_name="", n_samples: int = 3,
             textgen_config=TextGenerationConfig(n=1),
             summary_method: str = "default", encoding: str = 'utf-8') -> dict:
         """Summarize data from a pandas DataFrame or a file location"""
-
+ 
         # if data is a file path, read it into a pandas DataFrame, set file_name to the file name
         if isinstance(data, str):
             file_name = data.split("/")[-1]
             # modified to include encoding
             data = read_dataframe(data, encoding=encoding)
         data_properties = self.get_column_properties(data, n_samples)
-
+ 
         # default single stage summary construction
         base_summary = {
             "name": file_name,
@@ -136,9 +170,9 @@ class Summarizer():
             "dataset_description": "",
             "fields": data_properties,
         }
-
+ 
         data_summary = base_summary
-
+ 
         if summary_method == "llm":
             # two stage summarization with llm enrichment
             data_summary = self.enrich(
@@ -152,8 +186,9 @@ class Summarizer():
                 "file_name": file_name,
                 "dataset_description": ""
             }
-
+ 
         data_summary["field_names"] = data.columns.tolist()
         data_summary["file_name"] = file_name
-
+ 
         return data_summary
+ 
