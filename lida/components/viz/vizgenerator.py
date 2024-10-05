e9f78d13512279b535885ea5d3765d72c1de900b
allainerain <allainericci@gmail.com>
2024-10-05 16:38:15 +0800

    refactor: clean-up code for vis generator

--- lida/components/viz/vizgenerator.py
+++ lida/components/viz/vizgenerator.py
@@ -7,7 +7,9 @@ from lida.datamodel import Goal
 
 
 system_prompt = """
-You are a helpful assistant highly skilled in writing PERFECT code for visualizations. Given some code template, you complete the template to generate a visualization given the dataset and the goal described. The code you write MUST FOLLOW VISUALIZATION BEST PRACTICES ie. meet the specified goal, apply the right transformation, use the right visualization type, use the right data encoding, and use the right aesthetics (e.g., ensure axis are legible). The transformations you apply MUST be correct and the fields you use MUST be correct. The visualization CODE MUST BE CORRECT and MUST NOT CONTAIN ANY SYNTAX OR LOGIC ERRORS (e.g., it must consider the field types and use them correctly). 
+You are a helpful assistant highly skilled in writing PERFECT code for visualizations. 
+Given some code template, you complete the template to generate a visualization given the dataset and the goal described. 
+The code you write MUST FOLLOW VISUALIZATION BEST PRACTICES ie. meet the specified goal, apply the right transformation, use the right visualization type, use the right data encoding, and use the right aesthetics (e.g., ensure axis are legible). The transformations you apply MUST be correct and the fields you use MUST be correct. The visualization CODE MUST BE CORRECT and MUST NOT CONTAIN ANY SYNTAX OR LOGIC ERRORS (e.g., it must consider the field types and use them correctly). 
 You MUST first generate a brief plan for how you would solve the task, including what transformations you would apply (e.g., if you need to construct a new column, what fields you would use, what visualization type you would use, what aesthetics you would use, etc).
 """
 
@@ -32,7 +34,7 @@ class VizGenerator(object):
             library_instructions,
             {"role": "user",
              "content":
-             f"Always add a legend with various colors where appropriate. The visualization code MUST only use data fields that exist in the dataset (field_names) or fields that are transformations based on existing field_names). Only use variables that have been defined in the code or are in the dataset summary. You MUST return a FULL PYTHON PROGRAM ENCLOSED IN BACKTICKS ``` that starts with an import statement. DO NOT add any explanation. \n\n THE GENERATED CODE SOLUTION SHOULD BE CREATED BY MODIFYING THE SPECIFIED PARTS OF THE TEMPLATE BELOW \n\n {library_template} \n\n.The FINAL COMPLETED CODE BASED ON THE TEMPLATE above is ... \n\n"}]
+             f"Always add a legend with various colors IF appropriate. The visualization code MUST only use data fields that exist in the dataset (field_names) or fields that are transformations based on existing field_names). Only use variables that have been defined in the code or are in the dataset summary. You MUST return a FULL PYTHON PROGRAM ENCLOSED IN BACKTICKS ``` that starts with an import statement. DO NOT add any explanation. \n\n THE GENERATED CODE SOLUTION SHOULD BE CREATED BY MODIFYING THE SPECIFIED PARTS OF THE TEMPLATE BELOW \n\n {library_template} \n\n.The FINAL COMPLETED CODE BASED ON THE TEMPLATE above is ... \n\n"}]
 
         completions: TextGenerationResponse = text_gen.generate(
             messages=messages, config=textgen_config)
